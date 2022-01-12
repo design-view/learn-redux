f329eb351975ee74f86bb0900074dbfdd2113755
pink <pink@naver.com>
2022-01-12 16:17:30 +0900

    리덕스 모듈 완료

--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import reportWebVitals from './reportWebVitals';
-import './exercise'
-
+import rootReducer from './modules';
+const store = createStore(rootReducer);
+console.log(store.getState());
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
